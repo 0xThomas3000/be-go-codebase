47b19badf372e59a58b1e200e549a9c09df70906
tuanit168 <tuanit168@gmail.com>
2022-10-07 09:54:37 +0700

    08 - Adding base file

---
+++ api/controllers/base.go
@@ -0,0 +1,78 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/0xThomas3000/be-go-codebase/api/middlewares"
+
+	"github.com/0xThomas3000/be-go-codebase/api/models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	"gorm.io/driver/mysql"    //mysql database driver
+	"gorm.io/driver/postgres" //postgres database driver
+)
+
+/*
+a. Last steps (Controllers and Routes):
+  - How all these things connect right?
+  - These steps was purposely skipped till now cuz it calls most of the functions/methods we defined above.
+
+b. This "base" file:
+  - Will have our database connection information, call our routes, and start our server:
+*/
+type Server struct {
+	DB     *gorm.DB
+	Router *gin.Engine
+}
+
+var errList = make(map[string]string)
+
+func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
+
+	var err error
+
+	// If you are using mysql, i added support for you here(dont forgot to edit the .env file)
+	if Dbdriver == "mysql" {
+		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+		server.DB, err = gorm.Open(mysql.Open(DBURL), &gorm.Config{})
+		if err != nil {
+			fmt.Printf("Cannot connect to %s database", Dbdriver)
+			log.Fatal("This is the error:", err)
+		} else {
+			fmt.Printf("We are connected to the %s database", Dbdriver)
+		}
+	} else if Dbdriver == "postgres" {
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+		server.DB, err = gorm.Open(postgres.Open(DBURL), &gorm.Config{})
+		if err != nil {
+			fmt.Printf("Cannot connect to %s database", Dbdriver)
+			log.Fatal("This is the error connecting to postgres:", err)
+		} else {
+			fmt.Printf("We are connected to the %s database", Dbdriver)
+		}
+	} else {
+		fmt.Println("Unknown Driver")
+	}
+
+	//database migration
+	server.DB.Debug().AutoMigrate(
+		&models.User{},
+		&models.Post{},
+		&models.ResetPassword{},
+		&models.Like{},
+		&models.Comment{},
+	)
+
+	server.Router = gin.Default()
+	server.Router.Use(middlewares.CORSMiddleware())
+
+	server.initializeRoutes()
+
+}
+
+func (server *Server) Run(addr string) {
+	log.Fatal(http.ListenAndServe(addr, server.Router))
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/matcornic/hermes/v2 v2.1.0
 	github.com/sendgrid/sendgrid-go v3.12.0+incompatible
 	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b
+	gorm.io/driver/mysql v1.3.6
+	gorm.io/driver/postgres v1.3.10
 	gorm.io/gorm v1.23.10
 )
 
@@ -22,11 +24,20 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.12.0 // indirect
+	github.com/jackc/pgx/v4 v4.17.2 // indirect
 	github.com/jaytaylor/html2text v0.0.0-20211105163654-bc68cce691ba // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
